8181bf7fa87bba314be742ee00e4e424fd8e31f0
Eryk Dobrosz <eryk.dobrosz@gmail.com>
2019-03-12 00:35:17 +0100

    Unity showcase

---
+++ AndroidProject/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float movementSpeed = 2.0f;
+
+    public float Horizontal { get; set; } = 0.0f;
+    public float Vertical { get; set; } = 0.0f;
+
+    private Rigidbody2D _rigidbody2D;
+
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        var currentVelocity = new Vector2(Horizontal, Vertical).normalized * movementSpeed;
+        _rigidbody2D.MovePosition(_rigidbody2D.position + currentVelocity * Time.fixedDeltaTime);
+        //transform.position += currentVelocity * Time.deltaTime;
+    }
+}
---
+++ AndroidProject/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    private CharacterMovement _charMovement;
+
+    void Start()
+    {
+        _charMovement = GetComponent<CharacterMovement>();
+    }
+
+    void Update()
+    {
+        _charMovement.Horizontal = Input.GetAxis("Horizontal");
+        _charMovement.Vertical = Input.GetAxis("Vertical");
+    }
+}
