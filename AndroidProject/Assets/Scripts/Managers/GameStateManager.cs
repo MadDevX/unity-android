80fd3a2d506810c45a2b218e6806c37e33e1f473
Eryk Dobrosz <eryk.dobrosz@gmail.com>
2019-04-27 18:51:53 +0200

    Add event-based GameStateManager

---
+++ AndroidProject/Assets/Scripts/Managers/GameStateManager.cs
@@ -0,0 +1,64 @@
+﻿using Assets.Scripts.Enums;
+using System;
+using System.Collections.Generic;
+
+namespace Assets.Scripts.Managers
+{
+    class GameStateManager
+    {
+        private GameState _state = GameState.Menu;
+
+        private Dictionary<GameState, Action> _stateEvents = new Dictionary<GameState, Action>();
+
+        public GameState State
+        {
+            get
+            {
+                return _state;
+            }
+
+            set
+            {
+                GetEvent(value)?.Invoke();
+                _state = value;
+            }
+        }
+
+        public GameStateManager()
+        {
+            InitStateEventDictionary();
+        }
+
+        public void Subscribe(GameState state, Action method)
+        {
+            var evt = GetEvent(state);
+            evt += method;
+            SetEvent(state, evt);
+        }
+
+        public void Unsubscribe(GameState state, Action method)
+        {
+            var evt = GetEvent(state);
+            evt -= method;
+            SetEvent(state, evt);
+        }
+
+        private void InitStateEventDictionary()
+        {
+            foreach (GameState state in Enum.GetValues(typeof(GameState)))
+            {
+                _stateEvents.Add(state, null);
+            }
+        }
+
+        private Action GetEvent(GameState state)
+        {
+            return _stateEvents[state];
+        }
+
+        private void SetEvent(GameState state, Action evt)
+        {
+            _stateEvents[state] = evt;
+        }
+    }
+}
