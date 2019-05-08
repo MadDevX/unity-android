332f511d340a9a243efdfcb35f51b09b264f8d90
Eryk Dobrosz <eryk.dobrosz@gmail.com>
2019-05-08 21:48:58 +0200

    Reset velocity on player death

--- AndroidProject/Assets/Scripts/Player/States/PlayerStateDead.cs
+++ AndroidProject/Assets/Scripts/Player/States/PlayerStateDead.cs
@@ -4,9 +4,20 @@ using UnityEngine;
 
 public class PlayerStateDead : PlayerState
 {
+    private Rigidbody2D _rigidbody2D;
+
     public override void Tick()
     {
-        throw new System.NotImplementedException();
+    }
+
+    protected override void SetupReferences()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    protected override void Init()
+    {
+        _rigidbody2D.velocity = Vector2.zero;
     }
 
     protected override PlayerStates GetState()
