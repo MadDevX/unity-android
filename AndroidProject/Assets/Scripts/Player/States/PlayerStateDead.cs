2d6b7eeaeb629857efe5f2ad58cf569b88b6ae0c
Eryk Dobrosz <eryk.dobrosz@gmail.com>
2019-06-04 14:58:21 +0200

    Add player death animation

--- AndroidProject/Assets/Scripts/Player/States/PlayerStateDead.cs
+++ AndroidProject/Assets/Scripts/Player/States/PlayerStateDead.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerStateDead : PlayerState
 {
     private Rigidbody2D _rigidbody2D;
+    private Animator _anim;
 
     public override void Tick()
     {
@@ -13,11 +14,18 @@ public class PlayerStateDead : PlayerState
     protected override void SetupReferences()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
     }
 
     protected override void Init()
     {
         _rigidbody2D.velocity = Vector2.zero;
+        _anim.SetBool("IsDead", true);
+    }
+
+    protected override void Dispose()
+    {
+        _anim.SetBool("IsDead", false);
     }
 
     protected override PlayerStates GetState()
